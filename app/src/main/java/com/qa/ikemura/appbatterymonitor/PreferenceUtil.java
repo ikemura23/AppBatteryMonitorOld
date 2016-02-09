71d6a94994710ec3fb0faebf18bfedd60dda02a9
ikemura <kazutaka.ikemura@intel.com>
2016-02-09 14:49:19 +0900

    create class PreferenceUtil

--- app/src/main/java/com/qa/ikemura/appbatterymonitor/PreferenceUtil.java
+++ app/src/main/java/com/qa/ikemura/appbatterymonitor/PreferenceUtil.java
@@ -1,7 +1,30 @@
+
 package com.qa.ikemura.appbatterymonitor;
 
-/**
- * Created by administrator on 16/02/09.
- */
-public class PreferenceUtil {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+final class PreferenceUtils {
+
+    private static SharedPreferences getDefaultSharedPreferences(final Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    private static Context getContext() {
+        return ApplicationController.getInstance().getApplicationContext();
+    }
+
+    public static void saveString(String key, String value) {
+        SharedPreferences.Editor editor = getDefaultSharedPreferences(getContext()).edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    protected static String loadString(String key) {
+        return getDefaultSharedPreferences(getContext()).getString(key, null);
+    }
+
+    private PreferenceUtils() {
+    }
 }
