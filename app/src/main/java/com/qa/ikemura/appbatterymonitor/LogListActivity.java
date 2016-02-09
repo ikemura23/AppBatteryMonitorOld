6a50d760e3117951cadbfef37d0bb1e7a9d0fab3
ikemura <kazutaka.ikemura@intel.com>
2016-02-09 16:42:14 +0900

    LogListActivityのアクションバーにhomeボタン追加

--- app/src/main/java/com/qa/ikemura/appbatterymonitor/LogListActivity.java
+++ app/src/main/java/com/qa/ikemura/appbatterymonitor/LogListActivity.java
@@ -5,8 +5,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.qa.ikemura.appbatterymonitor.dummy.DummyContent;
@@ -43,6 +45,10 @@ public class LogListActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -93,5 +99,22 @@ public class LogListActivity extends AppCompatActivity
             detailIntent.putExtra(LogDetailFragment.ARG_ITEM_ID, item);
             startActivity(detailIntent);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            // This ID represents the Home or Up button. In the case of this
+            // activity, the Up button is shown. Use NavUtils to allow users
+            // to navigate up one level in the application structure. For
+            // more details, see the Navigation pattern on Android Design:
+            //
+            // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+            //
+            NavUtils.navigateUpTo(this, new Intent(this, LogListActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
