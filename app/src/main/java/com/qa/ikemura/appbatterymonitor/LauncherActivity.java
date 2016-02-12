7a54b2e6c0a48f5e5da6dd22124a6f4e693e2cc9
ikemura <kazutaka.ikemura@intel.com>
2016-02-12 14:36:13 +0900

    らんちゃーあくてぃヴぃてぃのりふぁくた

--- app/src/main/java/com/qa/ikemura/appbatterymonitor/LauncherActivity.java
+++ app/src/main/java/com/qa/ikemura/appbatterymonitor/LauncherActivity.java
@@ -1,22 +1,8 @@
 package com.qa.ikemura.appbatterymonitor;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Timer;
-import java.util.TimerTask;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -31,30 +17,9 @@ import android.widget.Toast;
 public class LauncherActivity extends AppCompatActivity {
 
     boolean isRecord = false;
-    Handler mHandler = new Handler();
-    // タイマー処理用
-    private Timer mTimer = null;
-    private int scale;
-    private int level;
-    private String fileName = null;
-    static final String mFilePath = Environment.getExternalStorageDirectory().getAbsolutePath()
-            + File.separator;
-
-    // 受信機
-    public BroadcastReceiver myReceiver = new BroadcastReceiver() {
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
-                // 電池残量の最大値
-                scale = intent.getIntExtra("scale", 0);
-                // 電池残量
-                level = intent.getIntExtra("level", 0);
-            }
-        }
-    };
 
     AppBatteryMonitorService appBatteryMonitorService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,32 +32,40 @@ public class LauncherActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.d("LauncherActivity", getString(getMessageId()));
+
+                //外部ストレージのチェック
+                if (isNotWriteStorage()) {
+                    return;
+                }
+                Log.d(Const.TAG, getString(getMessageId()));
                 Snackbar.make(view, getMessageId(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 isRecord = !isRecord;
+                //フローティングボタンのアイコン切り替え
                 changeFabSrc();
+                //ステータス文字の切り替え
                 setVisibleStatusText();
+                //ボタンを無効にして誤操作を防ぐ
                 setEnableOrDisableForButton();
 
                 if (isRecord) {
-                    // サンプルのサービス常駐を開始
+                    // サービス常駐を開始
                     appBatteryMonitorService.startResident(getApplicationContext());
-                    // startMonitor();
                 } else {
+                    // サービス停止
                     appBatteryMonitorService.stopResidentIfActive(getApplicationContext());
-                    // stopMonitor();
                 }
 
             }
         });
+        // 設定ボタン
         findViewById(R.id.activity_main_setting_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startSettingActivity();
             }
         });
-
+        // ログ一覧
         findViewById(R.id.activity_main_log_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -117,8 +90,7 @@ public class LauncherActivity extends AppCompatActivity {
         if (isRecord) {
             findViewById(R.id.activity_main_setting_button).setEnabled(false);
             findViewById(R.id.activity_main_log_button).setEnabled(false);
-        }
-        else {
+        } else {
             findViewById(R.id.activity_main_setting_button).setEnabled(true);
             findViewById(R.id.activity_main_log_button).setEnabled(true);
         }
@@ -127,30 +99,13 @@ public class LauncherActivity extends AppCompatActivity {
     private void setVisibleStatusText() {
         if (isRecord) {
             findViewById(R.id.activity_main_log_status_text).setVisibility(View.VISIBLE);
-        }
-        else {
+        } else {
             findViewById(R.id.activity_main_log_status_text).setVisibility(View.GONE);
         }
     }
 
     private int getMessageId() {
         return isRecord ? R.string.monitor_end_message : R.string.monitor_start_message;
-    }
-
-    /**
-     * 残バッテリー取得
-     * 
-     * @return 残バッテリー文字
-     */
-    private String getBatteryLevel() {
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Intent.ACTION_BATTERY_CHANGED);
-        Intent batteryStatus = registerReceiver(myReceiver, filter);
-        if (batteryStatus == null) {
-            return "failed";
-        }
-        int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
-        return "  L: " + String.valueOf(level);
     }
 
     /**
@@ -167,63 +122,6 @@ public class LauncherActivity extends AppCompatActivity {
         fab.setImageResource(resource);
     }
 
-    /**
-     * 記録開始
-     */
-    private void startMonitor() {
-
-        // マウント状態を確認
-        if (isNotMountSDCard()) {
-            Toast.makeText(this, "外部ストレージはマウントされてません",
-                    Toast.LENGTH_SHORT).show();
-            return;
-        }
-        // 外部ストレージの有無を確認
-        if (isNotExists()) {
-            Toast.makeText(this, "外部ストレージは存在しません", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        // 外部ストレージの状態を確認
-        if (isNotWrite()) {
-            Toast.makeText(this, "外部ストレージは書き込みできません", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        String intervalString = getInterval();
-        int interval = 15;
-        try {
-            interval = Integer.valueOf(intervalString);
-        } catch (Exception e) {
-            Log.d("LauncherActivity", e.getMessage());
-        }
-
-        // タイマー処理
-        mTimer = new Timer(true);
-        mTimer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                mHandler.post(new Runnable() {
-                    public void run() {
-                        // ここに処理を書く
-                        String FILENAME = mFilePath + getFileName();
-                        String strRecordTime = getRecordTime();
-                        String batteryLevel = getBatteryLevel();
-                        String value = strRecordTime + batteryLevel;
-
-                        // Log.d("LauncherActivity", FILENAME);
-                        Log.d("LauncherActivity", value);
-                        try {
-                            BufferedWriter bw = new BufferedWriter(new FileWriter(FILENAME, true));
-                            bw.write(value);
-                            bw.newLine();
-                            bw.close();
-                        } catch (Exception e) {
-                            Log.e("LauncherActivity", e.getMessage(), e);
-                        }
-                    }
-                });
-            }
-        }, 0, 1000 * 60 * interval); // 0秒後から, 設定 > intervalで設定した間隔で実行
-    }
 
     private String getInterval() {
         return PreferenceManager
@@ -231,9 +129,25 @@ public class LauncherActivity extends AppCompatActivity {
                 .getString(Const.INTERVAL_KEY, "15");
     }
 
-    private String getRecordTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss.SSS] ", Locale.JAPAN);
-        return sdf.format(new Date());
+    /**
+     * ログ出力可能？
+     *
+     * @return
+     */
+    public boolean isNotWriteStorage() {
+        if (isNotMountSDCard()) {
+            Toast.makeText(this, "SdCardがマウントされていません", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (isNotExists()) {
+            Toast.makeText(this, "外部ストレージが存在していません", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        if (isNotWrite()) {
+            Toast.makeText(this, "外部ストレージに書き込み不可能です", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -261,31 +175,6 @@ public class LauncherActivity extends AppCompatActivity {
      */
     private boolean isNotWrite() {
         return !Environment.getExternalStorageDirectory().canWrite();
-    }
-    /**
-     * ファイル名取得
-     * 
-     * @return
-     */
-    private String getFileName() {
-        if (fileName != null) {
-            return fileName;
-        }
-        Date now = new Date();
-        SimpleDateFormat f = new SimpleDateFormat("yyyyMMdd HH:mm:ss", Locale.getDefault());
-        fileName = f.format(now) + ".log";
-        return fileName;
-    }
-
-    /**
-     * 記録ストップ
-     */
-    private void stopMonitor() {
-        if (mTimer == null) {
-            return;
-        }
-        mTimer.cancel();
-        fileName = null;
     }
 
     // start activity ---------------------------------------------------------
